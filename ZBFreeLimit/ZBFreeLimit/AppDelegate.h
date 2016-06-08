e5fc5236322de21f0b81fdab9fed955f3bd5f633
千锋 <qianfeng@Krystal.local>
2016-06-08 14:24:37 +0800

    创建爱限免工程

---
+++ ZBFreeLimit/ZBFreeLimit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZBFreeLimit
+//
+//  Created by 千锋 on 16/6/8.
+//  Copyright (c) 2016年 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
