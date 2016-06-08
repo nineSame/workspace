ec182356dafdfb6afc77780fb7a7620e9fd772e3
千锋 <qianfeng@Krystal.local>
2016-06-08 14:46:18 +0800

    测试：加了一个视图控制器并且设置了首页背景颜色

---
+++ ZBFreeLimit/ZBFreeLimit/ZBViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZBViewController.h
+//  ZBFreeLimit
+//
+//  Created by 千锋 on 16/6/8.
+//  Copyright (c) 2016年 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZBViewController : UIViewController
+
+@end
